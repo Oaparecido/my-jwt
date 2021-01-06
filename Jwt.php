5f30df1146a5d8a6a6ebc1534bc6d0ab9f009bc6
Oaparecido <daniel.aparecido4683@gmail.com>
2021-01-06 17:14:41 -0300

    first commit - 2021/01/06

---
+++ Jwt.php
@@ -0,0 +1,34 @@
+<?php
+
+class Jwt {
+
+    private $payload = [
+        'name' => 'Daniel Apº'
+    ];
+
+    public function randString()
+    {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%*()';
+        $length_chars = strlen($chars);
+        $rand_string = '';
+        for ($i = 0; $i < 20; $i++) {
+            $rand_string .= $chars[rand(20, $length_chars - 1)];
+        }
+
+        return $rand_string;
+    }
+
+    public function sign()
+    {
+        $secret = $this->randString();
+        $header = base64_encode(json_encode(['alg' => 'SHA256', 'typ' => 'JWT']));
+        $payload = base64_encode(json_encode($this->payload));
+        $signature = $header . '.' . $payload . '.' . base64_encode($secret);
+
+        $signature = str_replace('=', '', $signature);
+        return $signature;
+    }
+}
+
+$object = new Jwt();
+echo $object->sign();
