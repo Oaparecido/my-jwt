b4892d3771e7bad00e995e058249c23988125724
Oaparecido <daniel.aparecido4683@gmail.com>
2021-01-06 23:03:33 -0300

    Beuatiful CLI

--- index.php
+++ index.php
@@ -10,18 +10,27 @@ $object = new Jwt([
 ]);
 
 $encrypt = $object->sign();
+$verification = $object->validate($encrypt);
 
 echo "|----------------------------|" . PHP_EOL;
 echo "|     --[(* My JWT *)]--     |" . PHP_EOL;
 echo "|----------------------------|" . PHP_EOL;
 echo "|                            |" . PHP_EOL;
 
-if ($object->validate($encrypt))
+if ($verification['payload'])
     echo "| [✅] Payload: \e[92mVerified\e[0m     |" . PHP_EOL;
 else
     echo "| [🚨] Payload: \e[91mNot verified\e[0m |" . PHP_EOL;
 
-echo "| [🚨] Header:  \e[91mNot verified\e[0m |" . PHP_EOL;
-echo "| [🚨] Secret:  \e[91mNot verified\e[0m |" . PHP_EOL;
+if ($verification['secret'])
+    echo "| [✅] Secret:  \e[92mVerified\e[0m     |" . PHP_EOL;
+else
+    echo "| [🚨] Secret:  \e[91mNot verified\e[0m |" . PHP_EOL;
+
+if ($verification['secret'])
+    echo "| [✅] Header:  \e[92mVerified\e[0m     |" . PHP_EOL;
+else
+    echo "| [🚨] Header:  \e[91mNot verified\e[0m |" . PHP_EOL;
+
 echo "|                            |" . PHP_EOL;
 echo "|----------------------------|" . PHP_EOL;
