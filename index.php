ee787099763fd1c29a12d2f24beb6dd1cac9d3fb
Oaparecido <daniel.aparecido4683@gmail.com>
2021-01-06 22:42:26 -0300

    Added else for not verified payload

--- index.php
+++ index.php
@@ -18,6 +18,8 @@ echo "|                            |" . PHP_EOL;
 
 if ($object->validate($encrypt))
     echo "| [✅] Payload: \e[92mVerified\e[0m     |" . PHP_EOL;
+else
+    echo "| [🚨] Payload: \e[91mNot verified\e[0m |" . PHP_EOL;
 
 echo "| [🚨] Header:  \e[91mNot verified\e[0m |" . PHP_EOL;
 echo "| [🚨] Secret:  \e[91mNot verified\e[0m |" . PHP_EOL;
