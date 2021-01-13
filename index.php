37c967d775997420a9fb215f96c903512bcc7016
Oaparecido <daniel.aparecido4683@gmail.com>
2021-01-13 19:21:12 -0300

    Remove old code

--- index.php
+++ index.php
@@ -14,37 +14,17 @@ $object = new Jwt([
 ]);
 
 $encrypt = $object->sign();
+$verification = $object->validate($encrypt);
+$payload_credential = $object->getPayload($encrypt);
 
-$verified = new Verified($encrypt, (new Jwt()));
+$verified = new Verified($verification);
+$payload = new Payload($payload_credential);
+
 $verified->getMessage();
-die();
-$payload = new Payload($object);
 $payload->getMessage();
-//$encrypt = $object->sign();
-//$verification = $object->validate($encrypt);
-//$payload = $object->getPayload($encrypt);
+$payload->getCompleteMessage();
 die();
 
-echo "  \e[34m[--(* Payload *)--]\e[0m" . PHP_EOL;
-echo PHP_EOL;
-
-foreach ($payload as $key => $value) {
-    if (strlen($value) >= 10)
-        $value_min = substr($value, 0, 10) . '...';
-
-    echo "  [O] Key: -------- \e[36m['" . $key . "']\e[0m  " . PHP_EOL;
-    echo "  [O] Message: ---- \e[36m['" . $value_min . "']\e[0m  " . PHP_EOL;
-
-}
-
-echo PHP_EOL;
-$full_line = readline('  Show complete message!? (y/n) ');
-echo PHP_EOL;
-if ($full_line == 'y')
-    foreach ($payload as $value)
-        echo "  [O] Message: \e[36m'" . $value . "'\e[0m  " . PHP_EOL;
-
-echo PHP_EOL . PHP_EOL;
 echo "  \e[34m[--(* Expire Token *)--]\e[0m" . PHP_EOL;
 
 $count = 0;
