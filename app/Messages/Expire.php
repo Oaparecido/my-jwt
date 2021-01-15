e6772110366090d8d296841086fd69770c2bc542
Oaparecido <daniel.aparecido4683@gmail.com>
2021-01-14 22:49:03 -0300

    Refactor get message in expire

--- app/Messages/Expire.php
+++ app/Messages/Expire.php
@@ -4,11 +4,31 @@
 namespace App\Messages;
 
 
+use App\Lib\Jwt;
+
 class Expire
 {
-    private $token;
+    private $object;
+
     public function __construct()
     {
+        $this->object = new Jwt();
+    }
 
+    public function getMessage($count, $token, $encrypt)
+    {
+        sleep(1);
+
+        if ($count % 2 === 0) {
+            echo PHP_EOL;
+            echo "  \e[33mRefresh Token...\e[0m" . PHP_EOL;
+            sleep(2);
+
+            $change = $this->object->verifyToken($token, $encrypt);
+
+            if ($change) {
+                echo "  [✅] Token modified!" . PHP_EOL;
+            }
+        }
     }
 }
